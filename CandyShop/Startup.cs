6941dcde6b40fa5f367e2b6ae607a4625a5a6c9e
ezgih <ezgihareket@hotmail.com>
2021-03-10 22:46:21 +0300

    Adding Connection String(17)

--- CandyShop/Startup.cs
+++ CandyShop/Startup.cs
@@ -1,7 +1,10 @@
 using CandyShop.Models;
+using CandyShop.Views;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -13,10 +16,20 @@ namespace CandyShop
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));//17.ADIM Adding Connection String
+
             services.AddControllersWithViews(); //1.ADIM Configuration and Routing
             services.AddScoped<ICategoryRepository, CategoryRepository>(); //6.ADIM Adding Repositories To Services
             services.AddScoped<ICandyRepository, CandyRepository>(); //6.ADIM Adding Repositories To Services
