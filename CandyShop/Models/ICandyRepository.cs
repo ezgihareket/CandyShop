e28674e47b4e27e44dfad026be32f6b241260fb5
ezgih <ezgihareket@hotmail.com>
2021-03-10 20:52:40 +0300

    Adding Interfaces(3)

---
+++ CandyShop/Models/ICandyRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//3.ADIM Adding Interfaces
+namespace CandyShop.Models
+{
+    public interface ICandyRepository
+    {
+        IEnumerable<Candy> getAllCandy { get; }
+        IEnumerable<Candy> getCandyOnSale { get; }
+        Candy getCandyById(int candyId);
+    }
+}
---
+++ CandyShop/Models/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//3.ADIM Adding Interfaces
+namespace CandyShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> getAllCategories { get; }
+    }
+}
