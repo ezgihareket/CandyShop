b73c3117eb3159f5b305a4cab66800f6ca03e19d
ezgih <ezgihareket@hotmail.com>
2021-03-10 20:57:16 +0300

    Adding Category Repository(4)

---
+++ CandyShop/Models/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//4.ADIM Adding Category Repository
+namespace CandyShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> getAllCategories => new List<Category>
+        {
+            new Category{categoryId=1, categoryName="Hard Candy", categoryDescription="Awesome"},
+            new Category{categoryId=2, categoryName="Chocolate Candy", categoryDescription="greate"},
+            new Category{categoryId=3, categoryName="Fruit Candy", categoryDescription="beautifule"},
+        };
+    }
+}
