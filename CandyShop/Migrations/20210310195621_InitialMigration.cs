024b46d9a3557b8533b3190a468e1cbcc92ab772
ezgih <ezgihareket@hotmail.com>
2021-03-10 22:57:35 +0300

    Creating Database Using Code First Migration(20)

---
+++ CandyShop/Migrations/20210310195621_InitialMigration.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandyShop.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    categoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    categoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    categoryDescription = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.categoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Candies",
+                columns: table => new
+                {
+                    candyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    imageUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    imageThumbnailUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    isOnSale = table.Column<bool>(type: "bit", nullable: false),
+                    isInStock = table.Column<bool>(type: "bit", nullable: false),
+                    categoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candies", x => x.candyId);
+                    table.ForeignKey(
+                        name: "FK_Candies_Categories_categoryId",
+                        column: x => x.categoryId,
+                        principalTable: "Categories",
+                        principalColumn: "categoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Candies_categoryId",
+                table: "Candies",
+                column: "categoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candies");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
