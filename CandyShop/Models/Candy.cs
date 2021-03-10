a8d345e23c8f81294809abec4a5b111796abdf12
ezgih <ezgihareket@hotmail.com>
2021-03-10 20:50:33 +0300

    Adding Models(2)

---
+++ CandyShop/Models/Candy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//2.ADIM Adding Models
+namespace CandyShop.Models
+{
+    public class Candy
+    {
+        public int candyId { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public string imageUrl { get; set; }
+        public string imageThumbnailUrl { get; set; }
+        public bool isOnSale { get; set; }
+        public bool isInStock { get; set; }
+        public int categoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ CandyShop/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//2.ADIM Adding Models
+namespace CandyShop.Models
+{
+    public class Category
+    {
+        public int categoryId { get; set; }
+        public string categoryName { get; set; }
+        public string categoryDescription { get; set; }
+        public List<Candy> Candies { get; set; }
+    }
+}
