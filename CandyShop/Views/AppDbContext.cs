a6b0adfed6d23de9e0ab7982db59af1882568387
ezgih <ezgihareket@hotmail.com>
2021-03-10 22:42:57 +0300

    Adding Entity Framework and DbContext(16)

---
+++ CandyShop/Views/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CandyShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//16.ADIM Adding Entity Framework and DbContext
+namespace CandyShop.Views
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+           base(options)
+        {
+
+        }
+
+        public DbSet<Candy> Candies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
