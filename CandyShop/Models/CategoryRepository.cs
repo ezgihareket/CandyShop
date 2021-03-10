2e78bb4fdba62b328e750ce3038bc1cdd58db827
ezgih <ezgihareket@hotmail.com>
2021-03-10 22:50:32 +0300

    Modifying Category Repository for Entity Framework(19)

--- CandyShop/Models/CategoryRepository.cs
+++ CandyShop/Models/CategoryRepository.cs
@@ -1,18 +1,21 @@
-﻿using System;
+﻿using CandyShop.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 //4.ADIM Adding Category Repository
+//19.ADIM Modifying Category Repository for Entity Framework
 namespace CandyShop.Models
 {
     public class CategoryRepository : ICategoryRepository
     {
-        public IEnumerable<Category> getAllCategories => new List<Category>
+        private readonly AppDbContext _appDbContext;
+        public CategoryRepository(AppDbContext appDbContext)
         {
-            new Category{categoryId=1, categoryName="Hard Candy", categoryDescription="Awesome"},
-            new Category{categoryId=2, categoryName="Chocolate Candy", categoryDescription="greate"},
-            new Category{categoryId=3, categoryName="Fruit Candy", categoryDescription="beautifule"},
-        };
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Category> getAllCategories => _appDbContext.Categories;
     }
 }
