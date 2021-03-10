7a8c11ac56337518e6d3d83e80a56e691046dfee
ezgih <ezgihareket@hotmail.com>
2021-03-10 22:06:28 +0300

    Adding Controllers and Injecting Services(7)

---
+++ CandyShop/Controllers/CandyController.cs
@@ -0,0 +1,22 @@
+﻿using CandyShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//7.ADIM Adding Controllers and Injecting Services
+namespace CandyShop.Controllers
+{
+    public class CandyController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CandyController(ICandyRepository candyRepository, ICategoryRepository categoryRepository)
+        {
+            _candyRepository = candyRepository;
+            _categoryRepository = categoryRepository;
+        }
+    }
+}
