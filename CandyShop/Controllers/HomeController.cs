85ed419062b857d264c94f02d2c39901b33579ed
ezgih <ezgihareket@hotmail.com>
2021-03-11 00:41:11 +0300

    Creating Home Controller(27)

---
+++ CandyShop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using CandyShop.Models;
+using CandyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//27.ADIM Creating Home Controller
+namespace CandyShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        public HomeController(ICandyRepository candyRepository)
+        {
+            _candyRepository = candyRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                CandyOnSale = _candyRepository.getCandyOnSale
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ CandyShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CandyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//27.ADIM Creating Home Controller
+
+namespace CandyShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Candy> CandyOnSale { get; set; }
+    }
+}
