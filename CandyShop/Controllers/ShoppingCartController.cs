2716d35344554e0242ba4e19ca4d81448f1e0f55
ezgih <ezgihareket@hotmail.com>
2021-03-12 21:27:35 +0300

    Creating Shopping Cart Controller(36)

---
+++ CandyShop/Controllers/ShoppingCartController.cs
@@ -0,0 +1,35 @@
+﻿using CandyShop.Models;
+using CandyShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//36.ADIM Creating Shopping Cart Controller
+namespace CandyShop.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ICandyRepository _candyRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartController(ICandyRepository candyRepository, ShoppingCart shoppingCart)
+        {
+            _candyRepository = candyRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+        public ViewResult Index()
+        {
+            _shoppingCart.shoppingCartItems = _shoppingCart.GetShoppingCartItems();
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
---
+++ CandyShop/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CandyShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//36.ADIM Creating Shopping Cart Controller
+namespace CandyShop.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
