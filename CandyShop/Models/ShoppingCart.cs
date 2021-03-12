230323f51beadcda117496008d04bc669c5f363b
ezgih <ezgihareket@hotmail.com>
2021-03-12 17:11:17 +0300

    Adding Shopping Cart(30)

---
+++ CandyShop/Models/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using CandyShop.Views;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//30.ADIM Adding Shopping Cart
+namespace CandyShop.Models
+{
+    public class ShoppingCart
+    {
+        private readonly AppDbContext _appDbContext;
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> shoppingCartItems { get; set; }
+
+        public ShoppingCart(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public static ShoppingCart getCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = services.GetService<AppDbContext>();
+            string cartId = session.GetString("cartId") ?? Guid.NewGuid().ToString();
+            session.SetString("cartId", cartId);
+
+            return new ShoppingCart(context)
+            {
+                ShoppingCartId = cartId
+            };
+        }
+    }
+}
