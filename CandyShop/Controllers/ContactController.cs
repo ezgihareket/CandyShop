3002d8c04d71c19037926d9c31e14a47ce7e674b
ezgih <ezgihareket@hotmail.com>
2021-03-13 17:29:02 +0300

    Creating Custom Email Tag Helper(45)

---
+++ CandyShop/Controllers/ContactController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//45.ADIM Creating Custom Email Tag Helper
+namespace CandyShop.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
