db844ab79db9f763669017a4bdabe6a8d58a5534
ezgih <ezgihareket@hotmail.com>
2021-03-13 16:39:06 +0300

    Category Menu View Component(43)

---
+++ CandyShop/Components/CategoryMenu.cs
@@ -0,0 +1,27 @@
+﻿using CandyShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//43.ADIM Category Menu View Component
+namespace CandyShop.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _categoryRepository.getAllCategories.OrderBy(c => c.categoryName);
+
+            return View(categories);
+        }
+    }
+}
